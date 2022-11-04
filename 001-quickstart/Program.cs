244b927a51339db1e4405164d0bbaf9fc52f5895
seesharprun <sidandrews@microsoft.com>
2022-11-04 09:17:44 -0400

    Update code samples

--- 001-quickstart/Program.cs
+++ 001-quickstart/Program.cs
@@ -17,7 +17,7 @@ using CosmosClient client = new(
 // <new_database> 
 // Database reference with creation if it does not already exist
 Database database = await client.CreateDatabaseIfNotExistsAsync(
-    id: "adventureworks"
+    id: "cosmicworks"
 );
 
 Console.WriteLine($"New database:\t{database.Id}");
@@ -27,7 +27,7 @@ Console.WriteLine($"New database:\t{database.Id}");
 // Container reference with creation if it does not alredy exist
 Container container = await database.CreateContainerIfNotExistsAsync(
     id: "products",
-    partitionKeyPath: "/category",
+    partitionKeyPath: "/categoryId",
     throughput: 400
 );
 
@@ -37,8 +37,9 @@ Console.WriteLine($"New container:\t{container.Id}");
 // <new_item> 
 // Create new object and upsert (create or replace) to container
 Product newItem = new(
-    id: "68719518391",
-    category: "gear-surf-surfboards",
+    id: "70b63682-b93a-4c77-aad2-65501347265f",
+    categoryId: "61dba35b-4f02-45c5-b648-c6badc0cbd79",
+    categoryName: "gear-surf-surfboards",
     name: "Yamba Surfboard",
     quantity: 12,
     sale: false
@@ -46,26 +47,26 @@ Product newItem = new(
 
 Product createdItem = await container.CreateItemAsync<Product>(
     item: newItem,
-    partitionKey: new PartitionKey("gear-surf-surfboards")
+    partitionKey: new PartitionKey("61dba35b-4f02-45c5-b648-c6badc0cbd79")
 );
 
-Console.WriteLine($"Created item:\t{createdItem.id}\t[{createdItem.category}]");
+Console.WriteLine($"Created item:\t{createdItem.id}\t[{createdItem.categoryName}]");
 // </new_item>
 
 // <read_item> 
 // Point read item from container using the id and partitionKey
 Product readItem = await container.ReadItemAsync<Product>(
-    id: "68719518391",
-    partitionKey: new PartitionKey("gear-surf-surfboards")
+    id: "70b63682-b93a-4c77-aad2-65501347265f",
+    partitionKey: new PartitionKey("61dba35b-4f02-45c5-b648-c6badc0cbd79")
 );
 // </read_item>
 
 // <query_items> 
 // Create query using a SQL string and parameters
 var query = new QueryDefinition(
-    query: "SELECT * FROM products p WHERE p.category = @key"
+    query: "SELECT * FROM products p WHERE p.categoryId = @categoryId"
 )
-    .WithParameter("@key", "gear-surf-surfboards");
+    .WithParameter("@categoryId", "61dba35b-4f02-45c5-b648-c6badc0cbd79");
 
 using FeedIterator<Product> feed = container.GetItemQueryIterator<Product>(
     queryDefinition: query

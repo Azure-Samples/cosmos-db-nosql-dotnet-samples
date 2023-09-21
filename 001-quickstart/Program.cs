475ae78376f0275bd5d2a4928d2339879ad4df97
Sidney Andrews <sidandrews@microsoft.com>
2023-09-21 12:54:14 -0400

    Changed name of container in quickstart

--- 001-quickstart/Program.cs
+++ 001-quickstart/Program.cs
@@ -5,15 +5,15 @@ using Microsoft.Azure.Cosmos;
 // <client_credentials> 
 // New instance of CosmosClient class
 using CosmosClient client = new(
-    accountEndpoint: Environment.GetEnvironmentVariable("COSMOS_ENDPOINT")!,
-    authKeyOrResourceToken: Environment.GetEnvironmentVariable("COSMOS_KEY")!
+	accountEndpoint: Environment.GetEnvironmentVariable("COSMOS_ENDPOINT")!,
+	authKeyOrResourceToken: Environment.GetEnvironmentVariable("COSMOS_KEY")!
 );
 // </client_credentials>
 
 // <new_database> 
 // Database reference with creation if it does not already exist
 Database database = await client.CreateDatabaseIfNotExistsAsync(
-    id: "cosmicworks"
+	id: "cosmicworks"
 );
 
 Console.WriteLine($"New database:\t{database.Id}");
@@ -22,9 +22,9 @@ Console.WriteLine($"New database:\t{database.Id}");
 // <new_container> 
 // Container reference with creation if it does not already exist
 Container container = await database.CreateContainerIfNotExistsAsync(
-    id: "products",
-    partitionKeyPath: "/categoryId",
-    throughput: 400
+	id: "productsdemo",
+	partitionKeyPath: "/categoryId",
+	throughput: 400
 );
 
 Console.WriteLine($"New container:\t{container.Id}");
@@ -33,17 +33,17 @@ Console.WriteLine($"New container:\t{container.Id}");
 // <new_item> 
 // Create new object and upsert (create or replace) to container
 Product newItem = new(
-    id: "70b63682-b93a-4c77-aad2-65501347265f",
-    categoryId: "61dba35b-4f02-45c5-b648-c6badc0cbd79",
-    categoryName: "gear-surf-surfboards",
-    name: "Yamba Surfboard",
-    quantity: 12,
-    sale: false
+	id: "70b63682-b93a-4c77-aad2-65501347265f",
+	categoryId: "61dba35b-4f02-45c5-b648-c6badc0cbd79",
+	categoryName: "gear-surf-surfboards",
+	name: "Yamba Surfboard",
+	quantity: 12,
+	sale: false
 );
 
 Product createdItem = await container.CreateItemAsync<Product>(
-    item: newItem,
-    partitionKey: new PartitionKey("61dba35b-4f02-45c5-b648-c6badc0cbd79")
+	item: newItem,
+	partitionKey: new PartitionKey("61dba35b-4f02-45c5-b648-c6badc0cbd79")
 );
 
 Console.WriteLine($"Created item:\t{createdItem.id}\t[{createdItem.categoryName}]");
@@ -52,28 +52,28 @@ Console.WriteLine($"Created item:\t{createdItem.id}\t[{createdItem.categoryName}
 // <read_item> 
 // Point read item from container using the id and partitionKey
 Product readItem = await container.ReadItemAsync<Product>(
-    id: "70b63682-b93a-4c77-aad2-65501347265f",
-    partitionKey: new PartitionKey("61dba35b-4f02-45c5-b648-c6badc0cbd79")
+	id: "70b63682-b93a-4c77-aad2-65501347265f",
+	partitionKey: new PartitionKey("61dba35b-4f02-45c5-b648-c6badc0cbd79")
 );
 // </read_item>
 
 // <query_items> 
 // Create query using a SQL string and parameters
 var query = new QueryDefinition(
-    query: "SELECT * FROM products p WHERE p.categoryId = @categoryId"
+	query: "SELECT * FROM products p WHERE p.categoryId = @categoryId"
 )
-    .WithParameter("@categoryId", "61dba35b-4f02-45c5-b648-c6badc0cbd79");
+	.WithParameter("@categoryId", "61dba35b-4f02-45c5-b648-c6badc0cbd79");
 
 using FeedIterator<Product> feed = container.GetItemQueryIterator<Product>(
-    queryDefinition: query
+	queryDefinition: query
 );
 
 while (feed.HasMoreResults)
 {
-    FeedResponse<Product> response = await feed.ReadNextAsync();
-    foreach (Product item in response)
-    {
-        Console.WriteLine($"Found item:\t{item.name}");
-    }
+	FeedResponse<Product> response = await feed.ReadNextAsync();
+	foreach (Product item in response)
+	{
+		Console.WriteLine($"Found item:\t{item.name}");
+	}
 }
 // </query_items>
